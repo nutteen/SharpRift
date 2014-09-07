c72fd0de2665eb5cdf34e1e507b6856110a0e37d
nutteen <nutteen@gmail.com>
2014-09-08 01:23:27 +0700

    [Sample] Add Sample code to call some functions in DLL (initialize and shutdown HMD)

--- Program.cs
+++ Program.cs
@@ -10,7 +10,29 @@ namespace SharpRift
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Initial commit");
+            VR.Initialize();
+
+            HMDisplay vrDevice;
+
+            if (VR.Detect() > 0)
+            {
+                // Create a link to the first VR device
+                vrDevice = VR.Create(0);
+            }
+            else
+            {
+                // Create Fake VR Device
+                vrDevice = VR.CreateDebug(HMDisplayType.DK2);
+            }
+
+            // Start Sensor
+            if (VR.ConfigureTracking(vrDevice, SensorCaps.Orientation | SensorCaps.Position | SensorCaps.YawCorrection, 0))
+            {
+                Console.WriteLine("Sensor has started");
+            }
+
+            VR.Destroy(vrDevice);
+            VR.Shutdown();
         }
     }
 }
