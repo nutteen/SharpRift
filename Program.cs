3995be41a9b58a364c9b04c86a248c8132e28c21
nutteen <nutteen@gmail.com>
2014-09-08 01:09:02 +0700

    [SharpRift] Add VS project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpRift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initial commit");
+        }
+    }
+}
